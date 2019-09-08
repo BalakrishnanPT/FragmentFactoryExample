1ff638af0ab0eae46a391a6f851886fa0d3f44b8
balakrishnanfn <bala@nfnlabs.in>
2019-09-08 20:24:15 +0530

    Added : Initial Commit

---
+++ app/src/main/java/in/balakrishnan/fragmentfactoryexample/MainActivity.java
@@ -0,0 +1,15 @@
+package in.balakrishnan.fragmentfactoryexample;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportFragmentManager().beginTransaction().replace(R.id.frameHolder, new FormFragment()).addToBackStack(null).commit();
+    }
+}
